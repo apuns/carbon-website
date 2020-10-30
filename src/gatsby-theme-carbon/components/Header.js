1cada5fe966ea12af02c76e2f96c23ff3791ce2a
Punshon <Amanda.Punshon@gov.bc.ca>
2020-10-30 09:13:00 -0700

    header spacing update

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    BC Public Service Agency<span>Web Guidelines</span>
+    BC PSA&nbsp;<span>Web Guidelines</span>
   </Header>
 );
 
