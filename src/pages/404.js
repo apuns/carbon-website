099c4cd42a9a0a10db13ef311f8a0988d638232b
Punshon <Amanda.Punshon@gov.bc.ca>
2020-10-30 11:44:01 -0700

    update links to 'content-strategy'

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FourOhFour } from 'gatsby-theme-carbon';
 
 const links = [
-  { href: '/all-about-carbon/what-is-carbon', text: 'All about Carbon' },
+  { href: '/content-strategy/what-is-carbon', text: 'All about Carbon' },
   { href: '/components/overview', text: 'Components overview' },
   { href: '/designing/get-started', text: 'Get started designing' },
   { href: '/developing/get-started', text: 'Get started developing' },
