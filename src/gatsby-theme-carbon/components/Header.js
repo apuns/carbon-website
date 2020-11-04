db6c2fb5f84de65a706ad6ff3c9a52f004f90afb
apuns <58612760+apuns@users.noreply.github.com>
2020-11-04 13:30:43 -0800

    align header with body text

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    BC PSA&nbsp;<span>Web Guidelines</span>
+    PSECI&nbsp;<span>Web Guidelines</span>
   </Header>
 );
 
